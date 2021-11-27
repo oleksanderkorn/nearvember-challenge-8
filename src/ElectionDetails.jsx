3375f501a2a0a21afb311071d5bff5e475449cd2
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2021-11-27 18:09:18 +0100

    More ui fixes.

--- src/ElectionDetails.jsx
+++ src/ElectionDetails.jsx
@@ -182,20 +182,62 @@ const ElectionDetails = ({
             </svg>
           </button>
         </div>
-        <div className="text-xl font-medium text-black">
-          {election.description}
+        <div className="text-xl font-medium text-black flex flex-row items-center justify-between">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 mr-1 flex-none"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+            />
+          </svg>
+          <p className="flex-grow">{election.description}</p>
           <button
-            className="disabled:opacity-50 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded float-right"
+            className="disabled:opacity-50 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded flex-none"
             onClick={voteForCandidate}
             disabled={isUserVoted || !selectedCandidate}
           >
             {isUserVoted ? "Already vote" : "Submit vote"}
           </button>
         </div>
-        <div className="text-md font-medium text-black text">
-          Created at: {formatDate(creationDate)}
+        <div className="text-md font-medium text-black text flex flex-row items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 mr-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <p>Created: {formatDate(creationDate)}</p>
         </div>
-        <div className="text-md font-medium text-black text">
+        <div className="text-md font-medium text-black text flex flex-row items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 mr-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
           {formatDate(startDate)} - {formatDate(endDate)}
         </div>
         <div className="text-xl font-medium text-black text-center">
